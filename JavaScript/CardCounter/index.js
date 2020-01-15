029850c830ea2fda97cda89c49d21ccb80619ce0
gabesharpton <gabriel.sharpton@gmail.com>
2020-01-15 17:31:30 -0600

    testing card counter

---
+++ JavaScript/CardCounter/index.js
@@ -0,0 +1,42 @@
+var count = 0;
+
+
+function cc(card) {
+  // Only change code below this line
+switch(card){
+  case 2: 
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++;
+    break;
+  case 7:
+  case 8:
+  case 9:
+    count;
+    break;
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+    count--;
+    break;
+}
+var move = " Hold";
+if(count >= 0){
+  return move = " Bet";
+}
+
+
+console.log(count)
+console.log(move)
+return count + move
+  // Only change code above this line
+}
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); 
+console.log(cc(2), cc(3), cc(7), cc('K'), cc('A'));
+
