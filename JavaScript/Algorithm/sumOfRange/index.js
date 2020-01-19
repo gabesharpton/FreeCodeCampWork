aaa4e3b6125cce11c3180cf186474b28cfa36d87
gabesharpton <gabriel.sharpton@gmail.com>
2020-01-19 17:31:24 -0600

    sum alg finished

--- JavaScript/Algorithm/sumOfRange/index.js
+++ JavaScript/Algorithm/sumOfRange/index.js
@@ -1,23 +1,9 @@
 function sumAll(arr) {
     let a = arr[0];
     let b = arr[1];
-    let newArr = []
-    if(a < b){
-        for(let i = a; i <= b; i++){
-            newArr.push(i)
-        }
-    }else{
-       for(let i = b; i <= a; i++){
-            newArr.push(i)
-        }
-    }
-        let len = newArr.length+1
-     let finalArr = newArr[0] + newArr[1] + newArr[2] + newArr[3]
-    
-    console.log(len)
-    
-  
-    return finalArr;
-  }
+    let numCount = Math.abs(a - b) + 1;
+    let sum = ((a + b) * numCount) / 2
+    return sum
+}
   
   console.log(sumAll([10, 5]));
