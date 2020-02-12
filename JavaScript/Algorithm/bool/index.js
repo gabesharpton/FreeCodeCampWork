2c9bf9963877abfae748cc1309fe72873a1a1f13
gabesharpton <gabriel.sharpton@gmail.com>
2020-02-11 23:15:24 -0600

    added a few more algorithms

---
+++ JavaScript/Algorithm/bool/index.js
@@ -0,0 +1,10 @@
+//check if value is a boolean primitive.
+
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+  
+    return bool === true || bool === false ? true : false;
+  }
+  
+  booWho(null);
+  
---
+++ JavaScript/Algorithm/findElement/index.js
@@ -0,0 +1,4 @@
+//given an array and function, find the first true value and return it
+function findElement(arr, func) {
+    return arr.find(func)
+  }
